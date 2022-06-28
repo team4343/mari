24ca7fd5731ceee054a5860f403106ad7c49041e
Bobbbay <abatterysingle@gmail.com>
2022-06-28 16:49:45 +0000

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nci.url = "github:yusdacra/nix-cargo-integration";
+  };
+  outputs = inputs:
+    inputs.nci.lib.makeOutputs {
+      root = ./.;
+
+      overrides.shell = common: prev: {
+        packages = prev.packages ++ (with common.pkgs; [(pkgs.callPackage ./nix/wpilib-toolchain.nix {})]);
+      };
+    };
+}
